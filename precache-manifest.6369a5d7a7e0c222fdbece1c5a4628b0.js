8c40702a50b4b81a3d93cbe198a6b427f7164c84
albukzek <albukzek@gmail.com>
2022-05-13 00:55:00 +0300

    first commit

---
+++ precache-manifest.6369a5d7a7e0c222fdbece1c5a4628b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7aacb96415d76ec8765c",
+    "url": "/css/app.591f2fa9.css"
+  },
+  {
+    "revision": "e7b799e9f5079c804d6220ad065da6ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7aacb96415d76ec8765c",
+    "url": "/js/app.60ccac3d.js"
+  },
+  {
+    "revision": "cf247553917bcae912d9",
+    "url": "/js/chunk-vendors.e41d549c.js"
+  },
+  {
+    "revision": "f11bce428d38fd9f4d6f0ba31c2acffc",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
